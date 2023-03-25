cb249e89497ad36ea0ae6a2011ed3bcace1f526a
PradnyaTawar <tawar.rani96@gmail.com>
2023-03-25 15:43:52 +0530

    product page

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
 import express from 'express';
 import data from './data.js';
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import productRouter from './routes/productRoutes.js';
+import seedRouter from './routes/seedRoutes.js';
 
+dotenv.config();
 const app = express();
+const MONGODB_URL = process.env.MONGODB_URL;
 
-app.get('/api/products', (req,res) =>{
-    res.send(data.products);
-});
+mongoose.connect(MONGODB_URL);
+mongoose.connection.on('connected', () => {
+    console.log('db connected');
+})
+app.use('/api/seed', seedRouter);
+app.use('/api/products', productRouter);
 
 app.get('/api/products/slug/:slug', (req, res) => {
     const product = data.products.find((x) => x.slug === req.params.slug);
@@ -24,7 +33,7 @@ app.get('/api/products/slug/:slug', (req, res) => {
       res.status(404).send({ message: 'Product Not Found' });
     }
   });
-  
+
 const port = process.env.PORT || 5000;
 app.listen(port, () =>{
     console.log(`server started at: ${port}`) 
